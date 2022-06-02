8b276388af487f43e8764b287dcb0e4941e88d4a
huang0308 <209410215@gms.tku.edu.tw>
2022-06-03 03:43:53 +0800

    TestFetchUseCors_xx.js -- working version

--- client_15/src/pages/TestFetchUseCors_15.js
+++ client_15/src/pages/TestFetchUseCors_15.js
@@ -1,28 +1,51 @@
 import axios from 'axios';
+import { useEffect } from 'react';
 
-const TestFetchUseCors_15 = async () => {
-    const response = await fetch('http://localhost:5000');
-    const data1 = await response.json();
-    console.log('fetch data1',data1);
-
-    const data2 = await axios.get('http://localhost:5000');
-    console.log('axios data2', data2.data);
-
-    const currentUser = {
-        "name":"huang03083",
-        "email":"2094102153@gms.tku.edu.tw",
-        "password":"secret3",
-    };
-
-    try {
-        const { data } = await axios.post(
-            'http://localhost:5000/api/v1/auth_15/register_15',
-            currentUser
-        );
-        console.log('register data',data);
-    } catch (err) {
-        console.log(err);
+const TestFetchUseCors_15 = () => {
+    const fetchData1 = async () => {
+        const response = await fetch('http://localhost:5000');
+        const data1 = await response.json();
+        console.log('fetch data1',data1);
     }
-};
+    
+    const fetchData2 = async () => {
+        const data2 = await axios.get('http://localhost:5000');
+        console.log('axios data2', data2.data);
+    }
+    
+    const fetchData3 = async () => {
+        const currentUser = {
+            "name":"huang03087",
+            "email":"2094102157@gms.tku.edu.tw",
+            "password":"secret7",
+        };
+    
+        try {
+            const { data } = await axios.post(
+                'http://localhost:5000/api/v1/auth_15/register_15',
+                currentUser
+            );
+            console.log('register data',data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    
 
+
+useEffect(() => {
+    fetchData1();
+}, []);
+
+useEffect(() => {
+    fetchData2();
+}, []);
+
+useEffect(() => {
+    fetchData3();
+}, []);
+
+    return <div></div>;
+
+};
 export default TestFetchUseCors_15;
--- client_15/src/pages/TestFetchUseProxy_15.js
+++ client_15/src/pages/TestFetchUseProxy_15.js
@@ -1,32 +1,60 @@
 import axios from 'axios';
+import { useEffect } from 'react';
 
-const TestFetchUseProxy_15 = async () => {
-    const resp = await fetch('/api/data.json');
-    const data_local_json = await resp.json();
-    console.log('local json data', data_local_json);
-
-    const response = await fetch('/api/v1');
-    const data1 = await response.json();
-    console.log('fetch data1',data1);
-
-    const data2 = await axios.get('/api/v1');
-    console.log('axios data2', data2.data);
-
-    const currentUser = {
-        "name":"huang03085",
-        "email":"2094102155@gms.tku.edu.tw",
-        "password":"secret5",
-    };
-
-    try {
-        const { data } = await axios.post(
-            '/api/v1/auth_15/register_15',
-            currentUser
-        );
-        console.log('register data',data);
-    } catch (err) {
-        console.log(err);
+const TestFetchUseProxy_15 = () => {
+    const fetchDataLocal = async () => {
+        const resp = await fetch('/api/data.json');
+        const data_local_json = await resp.json();
+        console.log('local json data', data_local_json);
     }
+    
+    const fetchData1 = async () => {
+        const response = await fetch('/api/v1');
+        const data1 = await response.json();
+        console.log('fetch data1',data1);
+    }
+
+    const fetchData2 = async () => {
+        const data2 = await axios.get('/api/v1');
+        console.log('axios data2', data2.data);
+    }
+
+    const fetchData3 = async () => {
+        const currentUser = {
+            "name":"huang03088",
+            "email":"2094102158@gms.tku.edu.tw",
+            "password":"secret8",
+        };
+    
+        try {
+            const { data } = await axios.post(
+                '/api/v1/auth_15/register_15',
+                currentUser
+            );
+            console.log('register data',data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    
+    useEffect(() => {
+        fetchDataLocal();
+    }, []);
+
+    useEffect(() => {
+        fetchData1();
+    }, []);
+    
+    useEffect(() => {
+        fetchData2();
+    }, []);
+    
+    useEffect(() => {
+        fetchData3();
+    }, []);
+    
+        return <div></div>;
+    
 };
 
 export default TestFetchUseProxy_15;
